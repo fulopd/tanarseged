1644472871e2797da77e80457a9b09f85e0985ef
fulopd <fulopdaniel@gmail.com>
2019-10-29 18:56:07 +0100

    Diak model

---
+++ TanarSeged/TanarSeged/Model/Diak.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TanarSeged.Model
+{
+    class Diak
+    {
+        public string Nev { get; set; }
+        public int Osztalyzat { get; set; }
+        public bool Hianyzas { get; set; }
+
+        public Diak(string nev, int osztalyzat = 0, bool hianyzas = false)
+        {
+            Nev = nev;
+            Osztalyzat = osztalyzat;
+            Hianyzas = hianyzas;
+        }
+
+        
+    }
+}
